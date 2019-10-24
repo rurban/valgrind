13b93472f020dc225b56fdd89766baebca11f8e7
Tamas K Lengyel <tamas@tklengyel.com>
2019-10-24 14:54:52 -0600

    Add new domctl version

--- coregrind/m_syswrap/syswrap-xen.c
+++ coregrind/m_syswrap/syswrap-xen.c
@@ -751,6 +751,7 @@ PRE(domctl)
    case 0x0000000f:
    case 0x00000010:
    case 0x00000011:
+   case 0x00000012:
 	   break;
    default:
       bad_intf_version(tid, layout, arrghs, status, flags,
